b4fb7dc43fb11a514486bae9415b92811330e493
Masud Hasan <masudhasamf@gmail.com>
2022-10-12 14:35:28 +0600

    some updates

--- src/components/SingleQuiz/SingleQuiz.jsx
+++ src/components/SingleQuiz/SingleQuiz.jsx
@@ -5,19 +5,31 @@ import { EyeIcon } from '@heroicons/react/24/solid'
 
 const SingleQuiz = ({ quiz }) => {
     const { question, options, correctAnswer, } = quiz;
-    console.log(quiz);
+    // console.log(quiz);
+
+    const seeCorrectAnswer = () => {
+        alert(correctAnswer)
+    }
+
+    const handelOptionsClick = (selected) => {
+        if (selected === correctAnswer) {
+            alert('your answer is right')
+        } else {
+            alert('wrong answer')
+        }
+    }
     return (
         <div>
-            <div className='w-10/12 mx-auto bg-slate-400 py-1 drop-shadow-2xl rounded-lg mb-4'>
+            <div className='w-10/12 mx-auto bg-slate-400 py-1 drop-shadow-2xl rounded-lg mb-8'>
                 <div className='quiz-question w-11/12 mx-auto'>
                     <h1 className='text-2xl text-white font-semibold'>{question}</h1>
-                    <button title='see correct answer'><EyeIcon className="h-6 w-6 text-black" /></button>
+                    <button onClick={seeCorrectAnswer} title='see correct answer'><EyeIcon className="h-6 w-6 text-white" /></button>
                 </div>
-                <div className='lg:grid grid-cols-2 gap-2 w-10/12 mx-auto mt-3 mb-8'>
-                    <button className='border-2 rounded-md w-46 h-20 text-black font-semibold'><small>{options[0]}</small></button>
-                    <button className='border-2 rounded-md w-46 h-20 text-black font-semibold'><small>{options[1]}</small></button>
-                    <button className='border-2 rounded-md w-46 h-20 text-black font-semibold'><small>{options[2]}</small></button>
-                    <button className='border-2 rounded-md w-46 h-20 text-black font-semibold'><small>{options[3]}</small></button>
+                <div className='grid xm:grid-cols-1  lg:grid-cols-2 gap-2 w-10/12 mx-auto mt-3 mb-2'>
+                    <button onClick={() => handelOptionsClick(options[0])} className='border-2 hover:bg-orange-400 rounded-md w-46 h-20 text-black font-semibold'><small>{options[0]}</small></button>
+                    <button onClick={() => handelOptionsClick(options[1])} className='border-2 hover:bg-orange-400 rounded-md w-46 h-20 text-black font-semibold'><small>{options[1]}</small></button>
+                    <button onClick={() => handelOptionsClick(options[2])} className='border-2 hover:bg-orange-400 rounded-md w-46 h-20 text-black font-semibold'><small>{options[2]}</small></button>
+                    <button onClick={() => handelOptionsClick(options[3])} className='border-2 hover:bg-orange-400 rounded-md w-46 h-20 text-black font-semibold'><small>{options[3]}</small></button>
                 </div>
             </div>
         </div>
--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -6,7 +6,7 @@ const Topics = () => {
     const loadedData = useLoaderData();
     const allTopics = loadedData.data;
     return (
-        <div className='lg:grid grid-cols-3 gap-8 w-11/12 mx-auto'>
+        <div className='grid xm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-11/12 mx-auto'>
             {
                 allTopics.map(topic => <Topic key={topic.id} topic={topic}></Topic>)
             }
